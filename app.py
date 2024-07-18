d557778feb7914409a42b9f45f490cf76ebbf6bb
Amit.Pawar1 <amit.pawar@rockwellautomation.com>
2024-07-18 20:20:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import PyPDF2 as pdf
+import google.generativeai as genai
+from scripts.constants.prompts import ats_prompt
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(input)
+    return response.text
+
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
+
+input_prompt=ats_prompt
+
+## streamlit app
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text=input_pdf_text(uploaded_file)
+        response=get_gemini_response(input_prompt)
+        st.subheader(response)
